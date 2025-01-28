dcc9c78ff5f40b94f005984eb75812923736abe9
22051988Di <vitalii.soloninko@gmail.com>
2025-01-28 10:49:27 +0100

    0.4	Components menu, create, update and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { MenuComponent } from './menu/menu.component';
+import { UpdateComponent } from './update/update.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'menu', pathMatch: 'full' },
+  { path: 'menu', component: MenuComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'update', component: UpdateComponent },
+];
